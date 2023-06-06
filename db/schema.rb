8c286ddcbdd0d4b7927322e99d89775c19f86486
johnie261 <njorogejohn261@gmail.com>
2023-06-06 17:57:34 +0300

    Create post migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_144627) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_145529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_144627) do
     t.datetime "updated_at", null: false
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "posts_counter", default: 0
   end
 
 end
