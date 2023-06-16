1fc0e43dd1f50dc6756bde6c9463f2bb4b3016cd
johnie261 <njorogejohn261@gmail.com>
2023-06-16 16:44:44 +0300

    Add new controller files and also modify current contoller files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,20 @@
-class CommmentsController < ApplicationController
+class CommentsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
+    @comment = current_user.comments.build(comment_params.merge(post: @post))
 
     if @comment.save
-        redirect_to @post
-      else
-        render 'new'
-      end
+      redirect_to user_post_path(@user, @post)
+    else
+      render 'new'
+    end
   end
 
   private
 
   def comment_params
-    #params.require(:comment).permit(:text)
-    params.require(:comment).permit(:text).merge(author: current_user)
+    # params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,21 +7,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
+    @user = @post.author
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to user_post_path(@post.author, @post)
+
     else
       render 'new'
     end
   end
-  
+
   private
 
   def post_params
     params.require(:post).permit(:title, :text)
   end
-  
 end
